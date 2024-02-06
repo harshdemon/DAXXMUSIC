5b83f942450b1984d9c4801ffb057a559cb66138
Anik <135734362+anikalwayspro@users.noreply.github.com>
2024-02-06 18:55:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://telegram.me/Catchme_here")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://telegram.me/anik_x_suoporttt")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False)
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
